8500a55c03e282f78032020adb42d6bb36328718
wn0858 <wn0858@gmail.com>
2018-02-20 01:10:37 -0500

    add min and max of period data

--- CoinDesk-line+chart.py
+++ CoinDesk-line+chart.py
@@ -1,49 +1,52 @@
-
-# coding: utf-8
-
-# In[79]:
-
 import json
 import requests
 import pandas as pd
-get_ipython().magic('matplotlib inline')
+# get_ipython().magic('matplotlib inline')
 import matplotlib.pyplot as plt
 
 
-# In[84]:
+class BitCoin:
 
-response = requests.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2017-09-01&end=2017-11-05')
+    def get_data_period(start,end):
+        response = requests.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=%s&end=%s'%(start,end));
+        # print(response);
+        json_data = json.loads(response.text);
+        # print (json_data);
+        json_bpi=json_data['bpi'];
+        # print (json_bpi)
+        arr_bpi_data=[];
+        for value in json_bpi.items():
+            # print (value[1]);
+            arr_bpi_data.append(value[1]);
+        # print(arr_bpi_data);
+        min_bpi_data=min(arr_bpi_data,key=float);
+        print(min_bpi_data);
+        max_bpi_data=max(arr_bpi_data,key=float);
+        print(max_bpi_data);
+        # data_line=pd.Series(bpi);
+        # print(data_line);
+        # plt.figure(figsize=(15,10));
+        # data_line.plot.line();
+        # plt.xlabel('Dates',fontsize=20, fontweight='bold');
+        # plt.ylabel('Prices',fontsize=20, fontweight='bold');
 
+    def get_data_30days():
+        response = requests.get('https://api.coindesk.com/v1/bpi/historical/close.json');
+        # print(response);
+        json_data = json.loads(response.text);
+        # print (json_data);
+        json_bpi=json_data['bpi'];
+        # print (json_bpi)
+        arr_bpi_data=[];
+        for value in json_bpi.items():
+            # print (value[1]);
+            arr_bpi_data.append(value[1]);
+        # print(arr_bpi_data);
+        min_bpi_data=min(arr_bpi_data,key=float);
+        print(min_bpi_data);
+        max_bpi_data=max(arr_bpi_data,key=float);
+        print(max_bpi_data);
 
-# In[85]:
-
-json_data = json.loads(response.text)
-
-
-# In[86]:
-
-print (json_data)
-
-
-# In[95]:
-
-bpi=json_data['bpi']
-
-
-# In[93]:
-
-data_line=pd.Series(bpi)
-
-
-# In[94]:
-
-plt.figure(figsize=(15,10))
-data_line.plot.line();
-plt.xlabel('Dates',fontsize=20, fontweight='bold')
-plt.ylabel('Prices',fontsize=20, fontweight='bold')
-
-
-# In[ ]:
-
-
+    # get_data_period("2018-02-01","2018-02-15");
+    get_data_30days();
 
